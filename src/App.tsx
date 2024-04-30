e390018fdd776e9e30a5d2c66df8dc808d8a4bb2
Lexarlab <alexiabezerra.c@gmail.com>
2024-04-30 08:07:15 -0300

    home

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Flex from './componetes/Flex'
+import Home from './paginas/home/Home'
+
+
+function App() {
+  return(
+    <>
+      <Home/>
+      {/*<Flex/>*/}
+    </>
+  )
+}
+
+export default App
